421dc38f464a1f31ccda738171e9213986e2039e
wiseone <ogboroge>
2022-11-16 23:09:26 +0100

    Authentication

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 import express, {json} from 'express';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 const app = express();
 
 import connectDatabase from './src/config/database.js';
 import itemRouter from './src/api/routes/item.routes.js';
-import userRouter from './src/api/routes/user.route.js'
+import userRouter from './src/api/routes/user.route.js';
+
 connectDatabase(app);
 
-app.use(json())
+app.use(json());
+app.use(cors());
 app.use(cookieParser());
 
 app.get('/', (req, res)=>{
--- src/api/middlewares/authorize.js
+++ src/api/middlewares/authorize.js
@@ -3,14 +3,14 @@ import jwt from 'jsonwebtoken';
 export const authorize = async (req, res, next)=>{
     try{
         const token = req.cookies.auth_token;
-    
+        
         if(!token) return res
         .status(400)
         .json({
             success: false, 
             msg:'Access denied! Sign in' 
         });
-    
+
         const verified = jwt.verify(token, process.env.USER_JWT_SECRET);
         req.user = verified;
         next();
