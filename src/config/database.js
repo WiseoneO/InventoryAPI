15042224843c0ad2d3fea20a52bea99fd66b313c
wiseone <ogboroge>
2022-11-18 12:28:06 +0100

    .

--- src/config/database.js
+++ src/config/database.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT  || 6000;
 const connectDatabase = async (app)=>{
     try{
         await mongoose.connect(
-            process.env.MONGO_LOCO,
+            process.env.DB_URI,
             ()=>{
                 console.log('Connected to database...');
                 app.listen(PORT, ()=>console.log(`Server listening on localhost:${PORT}...`));
